60a414b3ec1b318281ac5245e02f6fab1f2e1a0b
Radouane Aouini <radouaneloo.aouini@gmail.com>
2025-01-03 10:59:05 +0100

    interfaces et classe pour le calcul du poids des mouvements

---
+++ app/src/main/java/projet/poo/CalculPoids.java
@@ -0,0 +1,23 @@
+package projet.poo;
+
+public class CalculPoids implements PoidsMouvements{
+
+    @Override
+    public double calculLongeur1(Mouvement m) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'calculLongeur1'");
+    }
+
+    @Override
+    public double calculLongeur2(Mouvement m) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'calculLongeur2'");
+    }
+
+    @Override
+    public double calculLongeur3(Mouvement m) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'calculLongeur3'");
+    }
+
+}
---
+++ app/src/main/java/projet/poo/PoidsMouvements.java
@@ -0,0 +1,33 @@
+package projet.poo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface PoidsMouvements {
+
+    double calculLongeur1(Mouvement m);
+
+    double calculLongeur2(Mouvement m);
+
+    double calculLongeur3(Mouvement m);
+
+    default List<Double> calculerPoids(List<Mouvement> m) {
+        List<Double> res = new ArrayList<>();
+        for (Mouvement mouvement : m) {
+            switch (mouvement.getLongueur()) {
+                case 1:
+                    res.add(calculLongeur1(mouvement));
+                    break;
+                case 2:
+                    res.add(calculLongeur2(mouvement));
+                    break;
+                case 3:
+                    res.add(calculLongeur3(mouvement));
+                    break;
+                default:
+                    res.add(-1.0);
+            }
+        }
+        return res;
+    }
+}
